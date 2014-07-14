9dcd046b15896358bf89c4ffe939bb10f5f3ed7b
startupjing <jinglu111@gmail.com>
2014-07-14 15:26:30 +0800

    update

--- js/post.js
+++ js/post.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
     //***********************
     //**评论的代码也删掉哦***
     window.disqus_shortname = 'startupjing'; // required: replace example with your forum shortname
-    $('#disqus_container .comment').on('true',function(){
+    $('#disqus_container .comment').on('click',function(){
         $(this).html('loading...');
         var that = this;
         $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
